1315a53bbbc2bbd360ae57619964c6425c0a269c
Никита Педоренко <nikitnya1@gmail.com>
2019-06-01 12:27:38 +0300

    Added DistanceCalculator

---
+++ LAB5OP/LAB5OP/DistanceCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LAB5OP
+{
+    static class DistanceCalculator
+    {
+        const double PIx = 3.141592653589793;
+        const double RADIUS = 6378.16;
+
+        /// <summary>
+        /// Convert degrees to Radians
+        /// </summary>
+        public static double Radians(double x)
+        {
+            return x * PIx / 180;
+        }
+
+        /// <summary>
+        /// Calculate the distance between two places.
+        /// </summary>
+        public static double DistanceBetweenPlaces(double lat1, double lon1, double lat2, double lon2)
+        {
+            double dlon = Radians(lon2 - lon1);
+            double dlat = Radians(lat2 - lat1);
+
+            double a = (Math.Sin(dlat / 2) * Math.Sin(dlat / 2)) + Math.Cos(Radians(lat1)) * Math.Cos(Radians(lat2)) * (Math.Sin(dlon / 2) * Math.Sin(dlon / 2));
+            double angle = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return angle * RADIUS;
+        }
+    }
+}
