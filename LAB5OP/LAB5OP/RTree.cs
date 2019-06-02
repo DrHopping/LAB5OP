581f865ad1140490a5367382825acc23dfcdf0c4
Никита Педоренко <nikitnya1@gmail.com>
2019-06-02 10:17:27 +0300

    Added ctor to RTree

--- LAB5OP/LAB5OP/RTree.cs
+++ LAB5OP/LAB5OP/RTree.cs
@@ -2,6 +2,37 @@
 {
     public class RTree<T>
     {
+        // parameters of the tree
+        private const int DEFAULT_MAX_NODE_ENTRIES = 10;
+        internal int maxNodeEntries;
+        int minNodeEntries;
 
+        public RTree()
+        {
+            init();
+        }
+
+        public RTree(int MaxNodeEntries, int MinNodeEntries)
+        {
+            minNodeEntries = MinNodeEntries;
+            maxNodeEntries = MaxNodeEntries;
+            init();
+        }
+
+        private void init()
+        {
+
+            if (maxNodeEntries < 2)
+            {
+                maxNodeEntries = DEFAULT_MAX_NODE_ENTRIES;
+            }
+
+            // The MinNodeEntries must be less than or equal to (int) (MaxNodeEntries / 2)
+            if (minNodeEntries < 1 || minNodeEntries > maxNodeEntries / 2)
+            {
+                minNodeEntries = maxNodeEntries / 2;
+            }
+
+        }
     }
 }
