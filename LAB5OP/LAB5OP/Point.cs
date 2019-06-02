0a6382984732fc4b32acf6be8776e1d3497e6181
Никита Педоренко <nikitnya1@gmail.com>
2019-06-02 10:12:12 +0300

    Added Point class (3d)

---
+++ LAB5OP/LAB5OP/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace LAB5OP
+{
+    public class Point
+    {
+        private const int DIMENSIONS = 3;
+
+        internal float[] coordinates;
+
+        public Point(float x, float y, float z)
+        {
+            coordinates = new float[DIMENSIONS];
+            coordinates[0] = x;
+            coordinates[1] = y;
+            coordinates[2] = z;
+        }
+    }
+}
