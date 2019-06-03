8c66aeffba948c75bfd8a61e7df92f13e2066eb7
Никита Педоренко <nikitnya1@gmail.com>
2019-06-03 17:34:00 +0300

    Created Selector class

--- LAB5OP/LAB5OP/Place.cs
+++ LAB5OP/LAB5OP/Place.cs
@@ -1,12 +1,12 @@
 ﻿namespace LAB5OP
-{
+{ 
     class Place
     {
         public Cartesian Cartesian { get; set; }
         public Point Point { get; set; }
         public string Type { get; set; }
         public string Subtype { get; set; }
-        public string Name { get; set; }
+        public string  Name { get; set; }
         public string Address { get; set; }
         public Place(Cartesian cartesian, string type, string subtype, string name, string address)
         {
---
+++ LAB5OP/LAB5OP/Selector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LAB5OP
+{
+    class Selector
+    {
+        public List<Place> Places { get; set; }
+
+        private RTree<Place> tree = new RTree<Place>(); 
+        public Selector(List<Place> places)
+        {
+            this.Places = places;
+            AddPlacesToTree();
+        }
+        private void AddPlacesToTree()
+        {
+            foreach (var place in Places)
+            {
+                tree.Add(place.Point, place);
+            }
+        }
+
+        /// <summary>
+        /// Select places in region with center, radius and given parameters
+        /// </summary>
+        /// <param name="point">Center of region</param>
+        /// <param name="radius">Radius of region</param>
+        /// <param name="parameters">Parameters: type, subtype, name, address</param>
+        /// <returns>Places that satisfy conditions</returns>
+        public List<Place> SelectNearest(Point point, float radius, params string[] parameters)
+        {
+            var nearest = tree.Nearest(point, radius);
+            if (parameters.Length > 4) { throw new System.ArgumentException("To many arguments"); }
+            var 
+        }
+    }
+}
