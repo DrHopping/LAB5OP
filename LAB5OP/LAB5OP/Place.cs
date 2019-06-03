13efabfc2d6f0b3ef4941082962e61fdadb4db67
Никита Педоренко <nikitnya1@gmail.com>
2019-06-03 16:14:01 +0300

    Created Place class

--- LAB5OP/LAB5OP/Place.cs
+++ LAB5OP/LAB5OP/Place.cs
@@ -2,6 +2,18 @@
 { 
     class Place
     {
-
+        public Cartesian Cartesian { get; set; }
+        public Point Point { get; set; }
+        public string Type { get; set; }
+        public string Subtype { get; set; }
+        public string Address { get; set; }
+        public Place(Cartesian cartesian, string type, string subtype, string address)
+        {
+            this.Cartesian = cartesian;
+            this.Type = type;
+            this.Subtype = subtype;
+            this.Address = address;
+            this.Point = Converter.CartesianToSpherical(cartesian);
+        }
     }
 }
