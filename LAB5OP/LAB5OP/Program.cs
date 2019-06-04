82199533ca3ea5129e6e60ab7fb63a5577f06175
Никита Педоренко <nikitnya1@gmail.com>
2019-06-04 20:47:55 +0300

    Added printing to file

--- LAB5OP/LAB5OP/Program.cs
+++ LAB5OP/LAB5OP/Program.cs
@@ -8,36 +8,6 @@ namespace LAB5OP
 {
     class Program
     {
-        static void ConverterTest()
-        {
-            //float lat = 51.85035f;
-            //float lon = 4.351721f;
-            //var a = Converter.CartesianToSpherical(lat, lon);
-            //Console.WriteLine(a.Item1 + " " + a.Item2 + " " + a.Item3);
-            //var b = Converter.SphericalToCartesian(a.Item1, a.Item2, a.Item3);
-            //Console.WriteLine(b.Item1 + " " + b.Item2);
-        }
-        static void DistanceTest()
-        {
-            RTree<string> tree = new RTree<string>();
-            float lat1 = 51.85035f;
-            float lon1 = 4.351721f;
-            float lat2 = 51.05434f;
-            float lon2 = 3.717424f;
-            var cart1 = new Cartesian(lat1, lon1);
-            var cart2 = new Cartesian(lat2, lon2);
-
-
-            var p1 = Converter.CartesianToSpherical(cart1);
-            var p2 = Converter.CartesianToSpherical(cart2);
-
-            Console.WriteLine(DistanceCalculator.DistanceBetweenPlaces(cart1,cart2));
-
-            tree.Add(p1, "Brussels");
-
-            var a = tree.Nearest(p2, 99);
-            Console.ReadKey();
-        }
         static void Main(string[] args)
         {
             ArgsParser arguments = new ArgsParser(args);
@@ -49,6 +19,7 @@ namespace LAB5OP
             List<Place> selected = selector.SelectNearest(cartesian,radius,parameters);
             Writer wr = new Writer(selected);
             wr.PrintToConsole();
+            wr.PrintToFile("output.txt");
 
             Console.ReadKey();
         }
--- LAB5OP/LAB5OP/Writer.cs
+++ LAB5OP/LAB5OP/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace LAB5OP
 {
@@ -10,13 +11,26 @@ namespace LAB5OP
         {
             this.places = places;
         }
-
         public void PrintToConsole()
         {
             Console.WriteLine("We found next entities in the sector:");
             for (int i = 0; i < places.Count; i++)
             {
                 Console.WriteLine($"{i+1}. {places[i].ToString()}");
+            }
+        }
+        public void PrintToFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+                File.Create(fileName);
+
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                sw.WriteLine("We found next entities in the sector:");
+                for (int i = 0; i < places.Count; i++)
+                {
+                    sw.WriteLine($"{i + 1}. {places[i].ToString()}");
+                }
             }
         }
 
